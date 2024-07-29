711615336f3c046d739d689cca131ce92e721342
dvDaniel <113447778+devdaniel28@users.noreply.github.com>
2024-07-29 16:40:09 -0300

    ..

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// Number random
 function numberRadom(x, y) {
     return Math.floor(Math.random() * (y - x + 2) + x)
 }
@@ -5,16 +6,30 @@ function numberRadom(x, y) {
 let randomNumber = numberRadom(12345, 6789)
 console.log(randomNumber)
 
+// String Random
 function stringRandom(str) {
-   const arr = str.split("")
-   
-   let num = arr.length // Pegando a quantidade de caracteres da string para randomizar no loop
-   for (num - 1; num > 0; num--) { 
-     const randomS = Math.floor(Math.random() * (num + 1)) // randomS == String Random
-     [arr[num], arr[randomS]] = [arr[randomS], arr[num]]; // Usando desestruturação de arrays para randomizar melhor
-   }
-  return arr.join("") 
+  const arr = str.split("");
+  let num = arr.length; // Pegando a quantidade de caracteres da string para randomizar no loop
+  for (num -= 1; num > 0; num--) { 
+    let randomS = Math.floor(Math.random() * (num + 1)); // randomS == String Random
+    [arr[num], arr[randomS]] = [arr[randomS], arr[num]]; // Usando desestruturação de arrays para randomizar melhor
+  }
+  return arr.join(""); 
 }
 
-let randomString = stringRandom("abcdefghijklmnopqrstuvwxyz")
-console.log(randomString)
+const randomString = stringRandom("abcdefghijklmnopqrstuvwxyz");
+console.log(randomString);
+
+// Randomizando os Numeros e string com a mesma estrategia de randomização da string, teoricamente
+function generatePassword(senha) {
+  const randopassword = senha.split("");
+  let senhaLength = randopassword.length;
+  for (let i = senhaLength - 1; i > 0; i--) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+    [randopassword[i], randopassword[randomIndex]] = [randopassword[randomIndex], randopassword[i]];
+  }
+  return randopassword.join("");
+}
+
+let passwordGenerate = generatePassword(randomNumber + randomString);
+console.log(`A senha inteira e: ${passwordGenerate}`);  

477459bc1cf454ad8459a6467291d28868008f5f
dvDaniel <113447778+devdaniel28@users.noreply.github.com>
2024-08-12 16:33:23 -0300

    ..

--- src/script.js
+++ src/script.js
@@ -1,35 +1,28 @@
-// Number random
-function numberRadom(x, y) {
-    return Math.floor(Math.random() * (y - x + 2) + x)
-}
-
-let randomNumber = numberRadom(12345, 6789)
-console.log(randomNumber)
-
-// String Random
-function stringRandom(str) {
+function randomCaracteres(str) {
   const arr = str.split("");
-  let num = arr.length; // Pegando a quantidade de caracteres da string para randomizar no loop
-  for (num -= 1; num > 0; num--) { 
-    let randomS = Math.floor(Math.random() * (num + 1)); // randomS == String Random
-    [arr[num], arr[randomS]] = [arr[randomS], arr[num]]; // Usando desestruturação de arrays para randomizar melhor
+  let numberArray = arr.length; // Pegando a quantidade de caracteres da string para randomizar no loop
+  for (numberArray -= 1; numberArray > 0; numberArray--) { 
+    let caracteresRandom = Math.floor(Math.random() * (numberArray + 1)); // caracteresRandom == String Random
+    [arr[numberArray], arr[caracteresRandom]] = [arr[caracteresRandom], arr[numberArray]]; // Usando desestruturação de arrays para randomizar melhor
   }
   return arr.join(""); 
 }
 
-const randomString = stringRandom("abcdefghijklmnopqrstuvwxyz");
-console.log(randomString);
+const randomString = randomCaracteres("abcdefghijklmnopqrstuvwxyz")
+console.log(randomString)
 
-// Randomizando os Numeros e string com a mesma estrategia de randomização da string, teoricamente
-function generatePassword(senha) {
-  const randopassword = senha.split("");
-  let senhaLength = randopassword.length;
-  for (let i = senhaLength - 1; i > 0; i--) {
-    let randomIndex = Math.floor(Math.random() * (i + 1));
-    [randopassword[i], randopassword[randomIndex]] = [randopassword[randomIndex], randopassword[i]];
-  }
-  return randopassword.join("");
-}
+const randomNumber = randomCaracteres("1234567890")
+console.log(randomNumber)
 
-let passwordGenerate = generatePassword(randomNumber + randomString);
-console.log(`A senha inteira e: ${passwordGenerate}`);  
+const randomBigString = randomCaracteres("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+console.log(randomBigString)
+
+const randomPassword = randomCaracteres(randomNumber + randomBigString + randomString)
+console.log(`A senha inteira e: ${randomPassword}`)
+
+// condições das senhas 
+let characterValue = document.getElementById('charactervalue')
+
+let allowLargePrint = document.getElementById('allowlargeprint')
+
+let allowNumbers = document.getElementById('allownumbers')
